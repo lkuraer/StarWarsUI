87644ee030df7e9d8537e2ac7ec02e629b95effd
lkuraer <lkuraer@gmail.com>
2025-01-22 15:35:15 +0500

    partial add

---
+++ Sources/SWUI/Models/TabType.swift
@@ -0,0 +1,18 @@
+//
+//  SWTabType.swift
+//  otus_dz2
+//
+//  Created by Ruslan Sabirov on 07/01/25.
+//
+
+@available(iOS 18, macOS 14, *)
+public enum TabType: String, CaseIterable {
+    case people = "people"
+    case planets = "planets"
+    case starships = "starships"
+    
+    public var title: String {
+        return rawValue
+    }
+}
+
